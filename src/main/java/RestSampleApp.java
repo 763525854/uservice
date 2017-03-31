60081345ba355541ec99aa907851511981129bb2
763525854 <763525854@qq.com>
2017-03-31 18:20:18 +0800

    Add files via upload

---
+++ src/main/java/RestSampleApp.java
@@ -0,0 +1,10 @@
+package uservice;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestSampleApp {
+	public static void main(String[] args) {
+		SpringApplication.run(RestSampleApp.class, args);
+	}
+}
